4df5a7b3326ec009d1604c953c2f0e15d66d2d0f
Khanh Vu <58975846+vudinhnamkhanh@users.noreply.github.com>
2021-10-05 15:48:30 +0700

    Update Recharge-Card.php

--- public/client/Recharge-Card.php
+++ public/client/Recharge-Card.php
@@ -28,6 +28,16 @@
                                 <label class="col-sm-12 col-md-2 col-form-label">Mệnh giá:</label>
                                 <div class="col-sm-12 col-md-10">
                                     <select class="custom-select" id="menhgia" required="">
+                                        <option value="">-- Mệnh giá --</option>
+                                        <option value="10000">10,000đ</option>
+                                        <option value="20000">20,000đ</option>
+                                        <option value="30000">30,000đ</option>
+                                        <option value="50000">50,000đ</option>
+                                        <option value="100000">100,000đ</option>
+                                        <option value="200000">200,000đ</option>
+                                        <option value="300000">300,000đ</option>
+                                        <option value="500000">500,000đ</option>
+                                        <option value="1000000">1,000,000đ</option>
                                     </select>
                                 </div>
                             </div>
